fe2ab402a8332a32d3db8ae57c3f006970b902c8
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-10-29 22:32:31 +0100

    Placeholder comments added to application na runtemplate modules company linted by flake

--- plugins/modules/application.py
+++ plugins/modules/application.py
@@ -1,4 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2024, Dvořák Vítězslav <info@vitexsoftware.cz>
+
+
+DOCUMENTATION = """
+---
+module: application
+
+short_description: Manage applications in Multiflexi
+
+description:
+    - This module allows you to assign or remove applications to/from companies in Multiflexi.
+
+author:
+    - Vitex (@Vitexus)
+
+requirements:
+    - "python >= 3.9"
+
+version_added: "2.1.0"
+
+options:
+    state:
+        description:
+            - The state of the application.
+        required: true
+        type: str
+        choices: ['present', 'absent']
+    company:
+        description:
+            - The company associated with the application.
+        required: true
+        type: str
+    code:
+        description:
+            - The code of the application.
+        required: true
+        type: str
+    application:
+        description:
+            - The name of the application.
+        required: true
+        type: str
+"""
+
+EXAMPLES = """
+    - name: Assign application to company
+      vitexus.multiflexi.application:
+        state: present
+        company: ExampleCompany
+        code: APP123
+        application: ExampleApp
+
+    - name: Remove application from company
+      vitexus.multiflexi.application:
+        state: absent
+        company: ExampleCompany
+        code: APP123
+        application: ExampleApp
+"""
+
+RETURN = """
+    message:
+        description: The output message that the module generates.
+        type: str
+        returned: always
+    changed:
+        description: Whether the application was changed.
+        type: bool
+        returned: always
+"""
+
+
 from ansible.module_utils.basic import AnsibleModule
+
 
 def run_module():
     module_args = dict(
@@ -37,8 +113,10 @@ def run_module():
 
     module.exit_json(**result)
 
+
 def main():
     run_module()
+
 
 if __name__ == '__main__':
     main()
